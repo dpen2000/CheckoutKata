b54695f8d7455a1b211f963b2aed191092b7d2e7
dpen2000 <davidpendray@gmail.com>
2017-04-22 21:06:51 +0100

    Add failing test for scanning B and then B again which should result in special price. Since prices are already externalized from Checkout, it seemed OK to pass through new pricing rule (PriceForTwo = 45) at this point.

--- CheckoutKata/Tests.cs
+++ CheckoutKata/Tests.cs
@@ -13,11 +13,12 @@ namespace CheckoutKata
         [InlineData(new[] { "A" }, 50)]
         [InlineData(new[] { "B" }, 30)]
         [InlineData(new[] { "A", "B" }, 80)]
+        [InlineData(new[] { "B", "B" }, 45)]
         public void CanCalculateTotalPriceCorrectly(string[] itemCodesToScan, int expectedTotalPrice)
         {
             var itemToPriceDictionary = new Dictionary<string, PricingRules>();
             itemToPriceDictionary.Add("A", new PricingRules(50));
-            itemToPriceDictionary.Add("B", new PricingRules(30));
+            itemToPriceDictionary.Add("B", new PricingRules(30) { PriceForTwo = 45 });
             var checkout = new Checkout(itemToPriceDictionary);
             foreach (var itemCode in itemCodesToScan)
             {
