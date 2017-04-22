6a72f377a409027b2eeaf03f43ea687661bf84c1
dpen2000 <davidpendray@gmail.com>
2017-04-22 19:56:21 +0100

    Setup xunit

--- CheckoutKata/Tests.cs
+++ CheckoutKata/Tests.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CheckoutKata
 {
-    public class Class1
+    public class Tests
     {
+        [Fact]
+        public void Test()
+        {
+
+        }
     }
 }
