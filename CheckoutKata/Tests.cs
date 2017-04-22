60e9788d45945e3fbbe3194c31926054e6ca2a64
dpen2000 <davidpendray@gmail.com>
2017-04-22 22:11:56 +0100

    For completeness, handle product D

--- CheckoutKata/Tests.cs
+++ CheckoutKata/Tests.cs
@@ -18,12 +18,14 @@ namespace CheckoutKata
         [InlineData(new[] { "B", "B", "C" }, 65)]
         [InlineData(new[] { "A", "A", "A"}, 130)]
         [InlineData(new[] { "B", "A", "B" }, 95)]
+        [InlineData(new[] { "D" }, 15)]
         public void CanCalculateTotalPriceCorrectly(string[] itemCodesToScan, int expectedTotalPrice)
         {
             var itemToPriceDictionary = new Dictionary<string, PricingRules>();
             itemToPriceDictionary.Add("A", new PricingRules(50) { SpecialPrice = new SpecialPriceConfig(130, 3) });
             itemToPriceDictionary.Add("B", new PricingRules(30) { SpecialPrice = new SpecialPriceConfig(45, 2) });
             itemToPriceDictionary.Add("C", new PricingRules(20));
+            itemToPriceDictionary.Add("D", new PricingRules(15));
             var checkout = new Checkout(itemToPriceDictionary);
             foreach (var itemCode in itemCodesToScan)
             {
