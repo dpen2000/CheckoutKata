a04605c9e4c67fd7f5e1b9929ccdc9b6190ccc90
dpen2000 <davidpendray@gmail.com>
2017-04-24 22:37:19 +0100

    Move Tests into own project

--- CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata.Tests/CheckoutTests.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace CheckoutKata
+namespace CheckoutKata.Tests
 {
-    public class Tests
+    public class CheckoutTests
     {
         [Theory]
         [InlineData(new[] { "A" }, 50)]
